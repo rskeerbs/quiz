73752342d0ddde1a236349304bb268216f65ae8a
Romi Keerbs <rskeerbs@gmail.com>
2020-03-29 03:08:08 -0700

    changed quiz topic

---
+++ script.js
@@ -0,0 +1,15 @@
+//what do I want to do?
+
+//HTML set up page
+
+//quiz--display first question
+//timer
+//person must select answer before timer runs down
+//no answer selected--counted as wrong
+//score counter--displays whether answer is wrong or right
+//score counter--keeps track of right answers
+
+//next question is dislayed after answer or no response
+
+//jquery--must display questions using html and css commands
+//all done on a single page
