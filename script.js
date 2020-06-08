bcd9d625ace173d26d5897aa3c978b2258705d2a
Romi Keerbs <rskeerbs@gmail.com>
2020-06-07 22:42:00 -0700

    Working on code

--- script.js
+++ script.js
@@ -1,9 +1,130 @@
 //what do I want to do?
 
-//HTML set up page
+var questions = [
+    {question = "What does HTML stand for?",
+     answerA = "Homepage Text Maker Language",
+     answerB = "Hyper Text Markup Language",
+     answerC = "How Text Makes Language",
+     correct = "B"
+    },
+
+    {question = "What does CSS stand for?",
+     answerA = "Coding Style Sheet",
+     answerB = "Colorful Style Stuff",
+     answerC = "Cascading Style Sheets",
+     correct = "C"
+    },
+
+    {question = "Which HTML element will make the largest heading?",
+     answerA = "<h1>",
+     answerB = "<h3>",
+     answerC = "<h6>",
+     correct = "A"
+    },
+
+    {question = "What does <a> stand for?",
+     answerA = "Anchor tag",
+     answerB = "At tag",
+     answerC = "Allocate tag",
+     correct = "A"
+    },
+
+    {question = "What is the most important HTML element for a form?",
+     answerA = "<form>",
+     answerB = "<input>",
+     answerC = "<submit>",
+     correct = "B"
+    },
+
+    {question = "What is one way to make text bold?",
+     answerA = "<i>",
+     answerB = "<em>",
+     answerC = "<b>",
+     correct = "C"
+    },
+
+    {question = "What is the correct HTML for creating a hyperlink?",
+    answerA = "<a web='url'>link text</a>",
+    answerB = "<a url='url'>link text</a>",
+    answerC = "<a href='url'>link text</a>",
+    correct = "C"
+   },
+
+   {question = "What does HREF stand for?",
+    answerA = "Hypertext Reference",
+    answerB = "Hyperlink Reference",
+    answerC = "HyperURL Reference",
+    correct = "A"
+   },
+
+   {question = "How can you open a link in a new tab/browser window?",
+    answerA = "<a href='url' target='tab'>",
+    answerB = "<a href='url' target='_blank'>",
+    answerC = "<a href='url' target='open'>",
+    correct = "B"
+   },
+
+   {question = "What code inserts a line break?",
+    answerA = "<br>",
+    answerB = "<line>",
+    answerC = "<break>",
+    correct = "A"
+   },
+
+   {question = "What is the HTML for making a checkbox?",
+    answerA = "<input type='box'>",
+    answerB = "<input type='check'>",
+    answerC = "<input type='checkbox'>",
+    correct = "C"
+   },
+
+   {question = "Who makes Web standards?",
+   answerA = "Google",
+   answerB = "The World Wide Web Consortium",
+   answerC = "DARPA",
+   correct = "B"
+  },
+
+  {question = "How do you make a bulleted HTML list?",
+   answerA = "<ol> and <li>",
+   answerB = "<li> only",
+   answerC = "<ul> and <li>",
+   correct = "C"
+  },
+
+  {question = "What HTML adds a background color?",
+   answerA = "<body style=background:powderblue;>",
+   answerB = "<body background-color=powderblue;>",
+   answerC = "<body style='background-color:powderblue;'>",
+   correct = "C"
+  },
+]
+
+var userScore = 0;
+var timeLeft = 20;0
 
 //quiz--display first question
-//timer
+
+First question round displayed
+
+event listener button click
+
+if user answers question correctly
+
+"Correct!"
+
+userScore++
+
+else user answers incorrectly
+
+"Incorrect"
+
+else timer counts to zero(?--no button click)
+
+"No response"
+
+
+//timer--displays time
 //person must select answer before timer runs down
 //no answer selected--counted as wrong
 //score counter--displays whether answer is wrong or right
@@ -11,5 +132,7 @@
 
 //next question is dislayed after answer or no response
 
-//jquery--must display questions using html and css commands
+next question round displayed
+
+//javascript--must display questions using html and css commands
 //all done on a single page
